3ca3225ba405899586a8485cfff1f777081f807b
Ali Usman <long_cafe@yahoo.com>
2026-02-11 15:29:43 +0700

    gitignore

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -10,4 +10,5 @@ $routes->post('/api/login', 'Login::index');
 $routes->get('/api/visit/by-user/(:any)/(:any)', 'Visit::byUser/$1/$2');
 $routes->get('/api/visit/by-user-to-day', 'Visit::byUserToDay');
 $routes->get('/api/customers', 'Customer::listCustomers');
+$routes->post('/api/customers', 'Customer::addCustomer');
 $routes->get('/api/customers-cari/(:any)', 'Customer::cariCustomers/$1');
--- app/Controllers/Customer.php
+++ app/Controllers/Customer.php
@@ -20,4 +20,38 @@ class Customer extends BaseController
         $mc = new ModelCustomers();
         return $this->respond(['data' => $mc->select('*')->where('aktif', '1')->like('name', $kataPencarian)->orderBy('name', 'ASC')->findAll()]);
     }
+
+    public function addCustomer()
+    {
+        helper(['form']); // Load the form helper
+        $rules = [
+            'customer_id' => 'required|min_length[4]',
+            'name' => 'required|min_length[3]|max_length[50]',
+            'address' => 'required',
+            'latitude' => 'required',
+            'longitude' => 'required',
+            'phone' => 'required'
+            ];
+            
+            if (!$this->validate($rules)) return $this->fail($this->validator->getErrors(), 409);
+            $mc = new ModelCustomers();
+        $json = $this->request->getJSON();
+        $customer = $mc->select('customer_id')->where('customer_id', $json->customer_id)->first();
+        if($customer) return $this->fail('ID Customer yang anda masukkan sudah ada', 400);
+        $data = [
+            'customer_id' => $json->customer_id,
+            'name' => $json->name,
+            'address' => $json->address,
+            'latitude' => $json->latitude,
+            'longitude' => $json->longitude,
+            'phone' => $json->phone,
+            'aktif' => 1
+        ];
+        try {
+            $mc->insert($data);
+            return $this->respond($data, 201);
+        } catch (\Throwable $th) {
+            return $this->fail($th->getMessage(), $th->getCode());
+        }
+    }
 }
